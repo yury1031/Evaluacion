74ea51f7c27ffcf9b95aef4ba21121e118d31e84
yury1031 <yuryrueda2013@gmail.com>
2023-03-30 21:41:20 -0500

    Intentó nota mayor y menor 2

--- src/main/java/com/example/Evaluacion/Controlador/controladorEstudiantes.java
+++ src/main/java/com/example/Evaluacion/Controlador/controladorEstudiantes.java
@@ -28,9 +28,4 @@ public class controladorEstudiantes {
         return servicio.notaMayor(estu);
     }
 
-    @GetMapping("/notaMayor")
-    public Float notaMe(@RequestBody Estudiantes estu){
-        return servicio.notaMenor(estu);
-    }
-
 }
